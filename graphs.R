a65a4e68aaf30eb3560dc73af4bcaa838a6a095d
Malcolm Barrios <malcolmbarrios@arizona.edu>
2023-10-26 17:27:34 -0700

    Starting graphs

--- graphs.R
+++ graphs.R
@@ -1,7 +1,18 @@
 #Create Protorype graphs for the shiny app 
+#Load up packages 
+library(readr)
+library(ggplot2)
+library(lubridate)
 
 #Import fvariables.csv from the github files 
-library(readr)
 variables <- read_csv("variables.csv")
+#remove redundant number column I accidentally added when exporting the spreadsheet earlier
+cleandata = subset(variables, select = -...1)
 
-View(variables)
+View(cleandata)
+
+
+#start making graphs! 
+ggplot(data = cleandata, aes(x = datetime)) + 
+  geom_line(aes(y = atmospheric_pressure.value), color = "red") +
+  geom_line(aes(y = solar_radiation.value), color = "blue")
