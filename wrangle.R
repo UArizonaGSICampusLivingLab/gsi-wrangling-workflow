daf0e542e730f9863f15ab61d5d662c2ee844165
Eric Scott <scottericr@gmail.com>
2023-10-27 13:17:23 -0400

    move download and wrangling code to a function

--- wrangle.R
+++ wrangle.R
@@ -5,6 +5,7 @@ zentracloud::setZentracloudOptions(
 library(zentracloud)
 library(tidyverse)
 library(fs)
+source("R/gsi_get_data.R")
 
 # Read in already saved data ----------------------------------------------
 
@@ -19,43 +20,13 @@ if (file_exists("example_data.csv")) {
 }
 
 
-# Download new data -------------------------------------------------------
+# Download and wrangle new data---------------------------------------------
 
-devices <- c("z6-19484", "z6-20761", "z6-20764", "z6-20762", "z6-20763")
-
-readings_all <- 
-  # provide an "anonymous function" to map() to iterate over `devices`
-  map(devices, \(x) {
-    getReadings(
-      device_sn  = x,
-      start_time = format(prev_end, "%Y-%m-%d %H:%M:%S"), #should actually be timestep after prev_end
-      end_time   = format(lubridate::now(), "%Y-%m-%d %H:%M:%S")
-    )
-  }) |> set_names(devices)
-
-# Wrangling ---------------------------------------------------------------
-
-all_df <- 
-  readings_all |> 
-  # collapse list of lists to list of data frames
-  map(\(x) list_rbind(x, names_to = "sensor_port")) |> 
-  # collapse list of data frames to a single data frame
-  list_rbind(names_to = "device_sn") |> 
-  # some initial data wrangling
-  separate_wider_delim(sensor_port, delim = "_", names = c("sensor", "port")) |> 
-  mutate(
-    port = str_remove(port, "port") |> as.numeric(),
-    datetime = parse_date_time(datetime, orders = "%Y-%m-%d %H:%M:%S%z", exact = TRUE) |> 
-      with_tz("America/Phoenix")
-  ) |> 
-  filter(!is.na(sensor)) |> #There are some NAs for `sensor` in some versions of the wrangled data.  This removes them.
-  select(-timestamp_utc, -tz_offset) #redundant columns
-
-
+all_df <- gsi_get_data(start = prev_end)
 
 # Append saved data -------------------------------------------------------
 
 bind_rows(old_dat, all_df) |> 
-  distinct() |> 
+  distinct() |> #remove duplicate rows
   write_csv("example_data.csv")
 
