7e1bb2cb08e73843fc1ebb2bb236ca895e2b4332
Eric Scott <scottericr@gmail.com>
2024-01-30 11:03:17 -0700

    write function to work for a single device

--- notes/model-API.R
+++ notes/model-API.R
@@ -6,59 +6,63 @@ library(tidyverse)
 device_sn <- "z6-19484"
 port_num <- 1
 # model_type <- "ETo" #other option is ETr, so just hard-coded for now
-
-#Create a request
-req <- 
-  request("https://zentracloud.com/api/v3") |> 
-  req_url_path_append("get_env_model_data") |> 
-  req_url_query(
-    device_sn = device_sn,
-    model_type = "ETo", 
-    port_num = port_num, 
-    inputs = toJSON(list( #TODO hard code or get from site_info.csv?
-      elevation = 3,
-      latitude = 32.25,
-      wind_measurment_height = 2
-    ), auto_unbox  = TRUE)
-  ) |> 
-  req_headers(
-    Authorization = paste("Token", Sys.getenv("ZENTRACLOUD_TOKEN")),
-    accept = "application/json"
+gsi_get_eto <- function(device_sn, port_num = 1, elevation = 806, latitude = 32.25, wind_measurement_height = 2) {
+  #Create a request
+  req <- 
+    request("https://zentracloud.com/api/v4") |> 
+    req_url_path_append("get_env_model_data") |> 
+    req_url_query(
+      device_sn = device_sn,
+      model_type = "ETo", 
+      port_num = port_num, 
+      # inputs = '{"elevation": 806, "latitude": 32.25, "wind_measurement_height": 2}'
+      inputs = toJSON(list( #TODO hard code or get from site_info.csv?
+        elevation = elevation,
+        latitude = latitude,
+        wind_measurment_height = wind_measurement_height
+      ), auto_unbox  = TRUE)
     ) |> 
-  req_throttle(rate = 1/62) #ridiculously slow API limit
-
-#View the request
-req
-
-#Perform the request
-resp <- req |> 
-  req_perform()
-
-#Extract data from the request
-
-output <- resp |> 
-  #extremely annoying that response is not JSON like it claims
-  resp_body_string() |> 
-  #extremely annoying that there are invalid values (unquoted NaN) in the JSON text
-  str_replace_all(pattern = 'NaN', replacement = '"NaN"') |> 
-  fromJSON()
-
-#add metadata to data where appropriate
-out_df <- as_tibble(output$data$readings)
-out_df |> 
-  add_column(device_sn = device_sn, port_num = port_num) |> 
-  #rename to match other data columns
-  rename(
-    ETo.value = value,
-    ETo.error_flag = error_flag,
-    ETo.error_description = error_description
+    req_headers(
+      Authorization = paste("Token", Sys.getenv("ZENTRACLOUD_TOKEN")),
+      accept = "application/json"
     ) |> 
-  #TODO: wrangle datetime in the same way as the rest of the data
-  select(device_sn, port_num, datetime, starts_with("ETo"))
-  #TODO: will first row always be an error?  If so, remove it
-
+    req_throttle(rate = 1/62) #ridiculously slow API limit
+  
+  #View the request
+  req
+  
+  #Perform the request
+  resp <- req |> 
+    req_perform()
+  
+  #Extract data from the request
+  
+  output <- resp |> 
+    #extremely annoying that response is not JSON like it claims
+    resp_body_string() |> 
+    #extremely annoying that there are invalid values (unquoted NaN) in the JSON text
+    str_replace_all(pattern = 'NaN', replacement = '"NaN"') |> 
+    fromJSON()
+  
+  #add metadata to data where appropriate
+  out_df <- as_tibble(output$data$readings)
+  out_final <- out_df |> 
+    add_column(device_sn = device_sn, port = port_num) |> 
+    #rename to match other data columns
+    rename(
+      ETo.value = value,
+      ETo.error_flag = error_flag,
+      ETo.error_description = error_description
+    ) |> 
+    mutate(
+      datetime = parse_date_time(datetime, orders = "%Y-%m-%d %H:%M:%S%z", exact = TRUE) |> 
+        with_tz("America/Phoenix") |> as_date()
+    ) |> 
+    select(device_sn, port, datetime, starts_with("ETo"))
+  out_final
+}
 #Then repeat for every device_sn
+eto <- gsi_get_eto(device_sn = "z6-19484")
+eto
 
-#TODO create wrangle_response(resp)
-#TODO create gsi_get_eto(device_sn) that uses wrangle_response() to output a tibble
-#TODO add this to the main workflow so model data gets combined at the appropriate point?  OR add into gsi_get_data()?
+#TODO write this as a separate .csv file to Box
