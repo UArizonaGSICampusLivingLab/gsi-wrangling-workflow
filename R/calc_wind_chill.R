cc8fc374a898c106fb39f8aebe62584f3d33afb4
Eric Scott <scottericr@gmail.com>
2024-02-21 14:04:56 -0700

    move logic for when appropriate use to function

--- R/calc_wind_chill.R
+++ R/calc_wind_chill.R
@@ -1,4 +1,15 @@
-#TODO: some combinations of temp and wind speed give wind chill values *higher* than the real temperature.  Should probably check for that and replace those with the actual air temp.
+#' Calculate wind chill temperature
+#'
+#' Uses the national weather service equation
+#' https://www.weather.gov/media/epz/wxcalc/windChill.pdf. If calculated wind
+#' chill is greater than the real temperature, the real temperature is used. If
+#' the temperature is ≥ 50ºF and or wind speed is ≤ 3mph, then wind chill is not
+#' calculated and the real temperature is returned.
+#' 
+#' @param temp_f temperature in ºF
+#' @param wind_mph wind speed in miles per hour
+#'
+#' @return numeric vector of wind chill temps
 calc_wind_chill <- function(temp_f, wind_mph) {
   
   #NWS version
@@ -7,10 +18,12 @@ calc_wind_chill <- function(temp_f, wind_mph) {
   # from http://weather.uky.edu/aen599/wchart.htm
   # chill_f <- 0.0817 * (3.71 * sqrt(wind_mph) + 5.81 -0.25 * wind_mph) * (temp_f - 91.4) + 91.4
   
-  #check that chill_f is actually chillier.  If it isn't, just use the real temperature
-  
-  chill_f <- ifelse(chill_f > temp_f, temp_f, chill_f)
-  
+  #check that chill_f is actually chillier and that temp and wind speed are in appropriate range
+  chill_f <- ifelse(
+    test =  chill_f > temp_f | temp_f >= 50 | wind_mph <= 3,
+    yes = temp_f,
+    no = chill_f
+  )
   #return:
   chill_f
 }
